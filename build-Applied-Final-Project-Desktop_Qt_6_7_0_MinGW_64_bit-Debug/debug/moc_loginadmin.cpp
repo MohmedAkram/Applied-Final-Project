9036185dc9871871661222106b280957f33c4685
Mohamed Elsayed <mo.elsayed@aucegypt.edu>
2024-11-26 19:56:33 +0200

    push
    a

--- build-Applied-Final-Project-Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_loginadmin.cpp
+++ build-Applied-Final-Project-Desktop_Qt_6_7_0_MinGW_64_bit-Debug/debug/moc_loginadmin.cpp
@@ -35,7 +35,9 @@ namespace {
 #ifdef QT_MOC_HAS_STRINGDATA
 struct qt_meta_stringdata_CLASSLoginadminENDCLASS_t {};
 constexpr auto qt_meta_stringdata_CLASSLoginadminENDCLASS = QtMocHelpers::stringData(
-    "Loginadmin"
+    "Loginadmin",
+    "on_Login_clicked",
+    ""
 );
 #else  // !QT_MOC_HAS_STRINGDATA
 #error "qtmochelpers.h not found or too old."
@@ -48,12 +50,18 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSLoginadminENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   20,    2, 0x08,    1 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
 
        0        // eod
 };
@@ -66,16 +74,23 @@ Q_CONSTINIT const QMetaObject Loginadmin::staticMetaObject = { {
     nullptr,
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSLoginadminENDCLASS_t,
         // Q_OBJECT / Q_GADGET
-        QtPrivate::TypeAndForceComplete<Loginadmin, std::true_type>
+        QtPrivate::TypeAndForceComplete<Loginadmin, std::true_type>,
+        // method 'on_Login_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
     >,
     nullptr
 } };
 
 void Loginadmin::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    (void)_o;
-    (void)_id;
-    (void)_c;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Loginadmin *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->on_Login_clicked(); break;
+        default: ;
+        }
+    }
     (void)_a;
 }
 
@@ -95,6 +110,17 @@ void *Loginadmin::qt_metacast(const char *_clname)
 int Loginadmin::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 1;
+    }
     return _id;
 }
 QT_WARNING_POP
--- build-Applied-Final-Project-Desktop_Qt_6_7_0_MinGW_64_bit-Debug/ui_loginadmin.h
+++ build-Applied-Final-Project-Desktop_Qt_6_7_0_MinGW_64_bit-Debug/ui_loginadmin.h
@@ -21,44 +21,44 @@ QT_BEGIN_NAMESPACE
 class Ui_Loginadmin
 {
 public:
-    QLineEdit *lineEdit;
-    QLineEdit *lineEdit_2;
-    QLabel *label;
-    QLabel *label_2;
-    QPushButton *pushButton;
-    QPushButton *pushButton_2;
-    QLabel *label_3;
-    QLineEdit *lineEdit_3;
+    QLineEdit *adminId;
+    QLineEdit *Pass;
+    QLabel *AdminID;
+    QLabel *password;
+    QPushButton *Login;
+    QPushButton *Signup;
+    QLabel *Createaccount;
+    QLineEdit *LoginPage;
 
     void setupUi(QDialog *Loginadmin)
     {
         if (Loginadmin->objectName().isEmpty())
             Loginadmin->setObjectName("Loginadmin");
         Loginadmin->resize(407, 328);
-        lineEdit = new QLineEdit(Loginadmin);
-        lineEdit->setObjectName("lineEdit");
-        lineEdit->setGeometry(QRect(70, 100, 113, 21));
-        lineEdit_2 = new QLineEdit(Loginadmin);
-        lineEdit_2->setObjectName("lineEdit_2");
-        lineEdit_2->setGeometry(QRect(70, 160, 113, 21));
-        label = new QLabel(Loginadmin);
-        label->setObjectName("label");
-        label->setGeometry(QRect(70, 70, 131, 21));
-        label_2 = new QLabel(Loginadmin);
-        label_2->setObjectName("label_2");
-        label_2->setGeometry(QRect(70, 130, 121, 20));
-        pushButton = new QPushButton(Loginadmin);
-        pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(70, 200, 100, 32));
-        pushButton_2 = new QPushButton(Loginadmin);
-        pushButton_2->setObjectName("pushButton_2");
-        pushButton_2->setGeometry(QRect(220, 240, 100, 32));
-        label_3 = new QLabel(Loginadmin);
-        label_3->setObjectName("label_3");
-        label_3->setGeometry(QRect(70, 250, 121, 20));
-        lineEdit_3 = new QLineEdit(Loginadmin);
-        lineEdit_3->setObjectName("lineEdit_3");
-        lineEdit_3->setGeometry(QRect(140, 30, 113, 21));
+        adminId = new QLineEdit(Loginadmin);
+        adminId->setObjectName("adminId");
+        adminId->setGeometry(QRect(70, 100, 113, 21));
+        Pass = new QLineEdit(Loginadmin);
+        Pass->setObjectName("Pass");
+        Pass->setGeometry(QRect(70, 160, 113, 21));
+        AdminID = new QLabel(Loginadmin);
+        AdminID->setObjectName("AdminID");
+        AdminID->setGeometry(QRect(70, 70, 131, 21));
+        password = new QLabel(Loginadmin);
+        password->setObjectName("password");
+        password->setGeometry(QRect(70, 130, 121, 20));
+        Login = new QPushButton(Loginadmin);
+        Login->setObjectName("Login");
+        Login->setGeometry(QRect(70, 200, 100, 32));
+        Signup = new QPushButton(Loginadmin);
+        Signup->setObjectName("Signup");
+        Signup->setGeometry(QRect(220, 240, 100, 32));
+        Createaccount = new QLabel(Loginadmin);
+        Createaccount->setObjectName("Createaccount");
+        Createaccount->setGeometry(QRect(70, 250, 121, 20));
+        LoginPage = new QLineEdit(Loginadmin);
+        LoginPage->setObjectName("LoginPage");
+        LoginPage->setGeometry(QRect(140, 30, 113, 21));
 
         retranslateUi(Loginadmin);
 
@@ -68,13 +68,13 @@ public:
     void retranslateUi(QDialog *Loginadmin)
     {
         Loginadmin->setWindowTitle(QCoreApplication::translate("Loginadmin", "Dialog", nullptr));
-        lineEdit->setText(QString());
-        label->setText(QCoreApplication::translate("Loginadmin", "Username", nullptr));
-        label_2->setText(QCoreApplication::translate("Loginadmin", "Password", nullptr));
-        pushButton->setText(QCoreApplication::translate("Loginadmin", "Login", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("Loginadmin", "Signup", nullptr));
-        label_3->setText(QCoreApplication::translate("Loginadmin", "Create an account", nullptr));
-        lineEdit_3->setText(QCoreApplication::translate("Loginadmin", "Login Page", nullptr));
+        adminId->setText(QString());
+        AdminID->setText(QCoreApplication::translate("Loginadmin", "Admin Id", nullptr));
+        password->setText(QCoreApplication::translate("Loginadmin", "Password", nullptr));
+        Login->setText(QCoreApplication::translate("Loginadmin", "Login", nullptr));
+        Signup->setText(QCoreApplication::translate("Loginadmin", "Signup", nullptr));
+        Createaccount->setText(QCoreApplication::translate("Loginadmin", "Create an account", nullptr));
+        LoginPage->setText(QCoreApplication::translate("Loginadmin", "Login Page", nullptr));
     } // retranslateUi
 
 };
